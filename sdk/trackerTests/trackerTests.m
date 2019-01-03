08a48fd7abef66b59567a22113ff0c2629e5e640
Tim Slot <tim.slot@gmail.com>
2019-01-03 16:48:14 +0100

    replace project with a framework project

---
+++ sdk/trackerTests/trackerTests.m
@@ -0,0 +1,37 @@
+//
+//  trackerTests.m
+//  trackerTests
+//
+//  Created by Tim Slot on 03/01/2019.
+//  Copyright © 2019 O2MC I/O. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface trackerTests : XCTestCase
+
+@end
+
+@implementation trackerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
