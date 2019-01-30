31504d1ee3c2629be3568a07e17da84d03e37032
Tim Slot <tim.slot@gmail.com>
2019-01-30 14:45:52 +0100

    add public O2MTracker header

---
+++ sdk/O2MTracker/O2MTracker.h
@@ -0,0 +1,18 @@
+//
+//  O2MTracker.h
+//  O2MTracker
+//
+//  Created by Tim Slot on 03/01/2019.
+//  Copyright © 2019 O2MC I/O. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+//! Project version number for O2MTracker.
+FOUNDATION_EXPORT double O2MTrackerVersionNumber;
+
+//! Project version string for O2MTracker.
+FOUNDATION_EXPORT const unsigned char O2MTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <O2MTracker/PublicHeader.h>
+#import <O2MTracker/O2MC.h>
+
